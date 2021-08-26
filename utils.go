f4f36307f6aba4a0fd5ff46ee9112cb426f4c93a
Anar Imamaliev <a_imamaliev@gcore.lu>
2021-08-26 22:47:04 +0300

    func ContainsInt added

--- utils.go
+++ utils.go
@@ -8,3 +8,13 @@ func Contains(a []string, x string) bool {
   }
   return false
 }
+
+func ContainsInt(a []int, x int) bool {
+  for _, n := range a {
+     if x == n {
+        return true
+     }
+  }
+  return false
+}
+
